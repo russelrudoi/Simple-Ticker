038057639e68b89bb1f62519d2b781b10dd7817f
Ruslan Russel <selezen.ua30@gmail.com>
2022-07-20 23:17:43 +0300

    Главная страница
    Сделан каркас верстки главного экрана

--- template-sass/js/main.js
+++ template-sass/js/main.js
@@ -1,112 +1,27 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const itemCoin = document.querySelector('.coin__item');
-//     // const widthWindow = document.querySelector('.coin__inner').clientWidth;
-//     const widthWindow = document.documentElement.clientWidth;
-//     // const heightWindow = document.querySelector('.coin__inner').clientHeight;
-//     const heightWindow = document.documentElement.clientHeight;
-//     let vector = {
-//         x: 1.5,
-//         y: -2.5
-//     }
+window.addEventListener('DOMContentLoaded', () => {
 
-//     function posit() {
-//         let posY = Math.floor(Math.random() * heightWindow);
-//         let posX = Math.floor(Math.random() * widthWindow);
-//         return [posX, posY];
-//     }
-
-//     function anim() {
-//         const {
-//             left: x,
-//             top: y
-//         } = itemCoin.getBoundingClientRect();
-
-//         if (x < 0) vector.x *= -1;
-//         if (y < 0) vector.y *= -1;
-//         if (x > widthWindow) vector.x *= -1;
-//         if (y > heightWindow) vector.y *= -1;
-
-//         itemCoin.style.left = x + vector.x + 'px';
-//         itemCoin.style.top = y + vector.y + 'px';
-
-//         console.log(x, y, vector)
-
-//         requestAnimationFrame(anim)
-//     }
+    const itemCoins = document.querySelectorAll('.coin__item');
+    const widthWindow = document.querySelector('.coin__inner').clientWidth;
+    const heightWindow = document.querySelector('.coin__inner').clientHeight;
 
 
+    //Скрипт для рандомного расположения монеток
 
-//     // function anim(itemCoin) {
-//     //     let pos = posit();
-//     //     let p = 0;
-//     //     posX = pos[0];
-//     //     posY = pos[1];
+    itemCoins.forEach(item => {
+        setPosition(item)
+    })
 
-//     //     function fra() {
-//     //         posX++;
-//     //         itemCoin.style.left = posX + 'px';
+    function posit() {
+        let posY = Math.floor(Math.random() * heightWindow);
+        let posX = Math.floor(Math.random() * widthWindow);
+        return [posX, posY];
+    }
 
-//     //         function fraNeg() {
-//     //             posX--;
-//     //             itemCoin.style.left = posX + 'px';
-//     //         }
-//     //         // posY++;
-//     //         // itemCoin.style.top = posY + 'px';
+    function setPosition(itemCoin) {
+        let posX = posit()[0];
+        let posY = posit()[1];
+        itemCoin.style.left = posX + 'px';
+        itemCoin.style.top = posY + 'px';
+    }
 
-//     //         if (posX < widthWindow) {
-//     //             requestAnimationFrame(fra)
-//     //         } else if (posX == widthWindow) {
-//     //             requestAnimationFrame(fraNeg)
-//     //         }
-
-
-//     //     }
-//     //     fra()
-//     // }
-
-//     // itemCoins.forEach(item => {
-//     //     anim(item)
-
-//     // });
-
-
-//     // itemCoins.forEach(item => {
-//     //     item.addEventListener('', () => anim(item));
-
-//     // });
-
-//     // function animation() {
-//     //     let posY = Math.floor(Math.random() * widthWindow);
-//     //     let posX = Math.floor(Math.random() * widthWindow);
-//     //     // posX++;
-//     //     // item.style.left = posX + 'px';
-//     //     // if (posX > widthWindow) {
-//     //     //     posX--;
-//     //     //     item.style.left = posX + 'px';
-//     //     //     requestAnimationFrame(animation)
-
-//     //     // }
-//     //     if (posX < widthWindow) {
-//     //         posX++;
-//     //         itemCoin.style.left = posX + 'px';
-//     //         let id = setTimeOut(animation, 1)
-//     //     }
-
-//     //     // function anim(item) {
-
-//     //     //     // item.style.left = Math.random() * widthWindow + 'px';
-//     //     //     // item.style.top = Math.random() * widthWindow + 'px';
-//     //     //     posX++;
-//     //     //     item.style.left = posX + 'px'
-//     //     //     if (posX < widthWindow) {
-
-//     //     //         requestAnimationFrame(animation)
-//     //     //     }
-
-//     //     // }
-//     // }
-
-//     // animation()
-
-
-// });
+})
